520bb17b08e7d3fea14d1656ad111a9c77d35ff2
Igor Pavlov <gophan1992@gmail.com>
2018-10-27 15:52:30 +0300

    initial commit

---
+++ lib/nats_listener_core.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'nats_listener_core/version'
+require 'nats_listener_core/client_catcher'
+require 'nats_listener_core/client_logger'
+require 'nats_listener_core/abstract_client'
+require 'nats_listener_core/abstract_subscriber'
+module NatsListenerCore
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'nats_listener_core'
+
+token = ENV.fetch('CODECOV_TOKEN') { nil }
+unless token.nil?
+  require 'simplecov'
+  SimpleCov.start
+
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
